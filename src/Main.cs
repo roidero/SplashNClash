1d5ff07b271f4fdbc6aa3d954a0f17d6ef74376a
wasdeilonn <sjamsuddin.dilon@gmail.com>
2026-08-30 12:40:32 +0200

    coral prototype

--- src/Main.cs
+++ src/Main.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Polytopia;
 using Polytopia.Data;
 using PolytopiaBackendBase.Common;
+using Il2Gen = Il2CppSystem.Collections.Generic;
+using EnumsNET;
 
 namespace SplashNClash;
 
@@ -15,5 +17,82 @@ public static class Main
         modLogger = logger;
         modLogger.LogInfo("SplashNClash loaded");
         Harmony.CreateAndPatchAll(typeof(Main));
+        PolyMod.Loader.AddPatchDataType("tileEffect", typeof(TileData.EffectType));
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MapGenerator), nameof(MapGenerator.MakeOcean))]
+    private static void AddReefs(MapGenerator __instance, MapData map, GameState gameState, bool shouldConvertShallows = true)
+    {
+        foreach (TileData tile in map.tiles)
+        {
+            int chance = 0;
+            float modifier = 1f;
+            
+            if (tile.terrain == Polytopia.Data.TerrainData.Type.Ocean) chance = 40;
+            else if (tile.terrain == Polytopia.Data.TerrainData.Type.Water) chance = 20;
+
+            if (__instance.random.Next(0, 101) < chance * modifier)
+            {
+                tile.AddEffect(EnumCache<TileData.EffectType>.GetType("coralreef"));
+            }
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(Tile), nameof(Tile.Render), typeof (MapRenderContext))]
+    [HarmonyPatch(typeof(Tile), nameof(Tile.Render), typeof (MapRenderContext), typeof (SkinVisualsTransientData))]
+    private static void RenderReef(Tile __instance)
+    {
+        if (__instance.IsHidden)
+        {
+            return;
+        }
+        LogNuking(__instance);
+        ShowCoral(__instance, __instance.data.HasEffect(EnumCache<TileData.EffectType>.GetType("coralreef")));
+    }
+
+    public static void LogNuking(Tile tile)
+    {
+        GameObject gobj = tile.gameObject;
+        if (gobj != null)
+        {
+            foreach (SpriteRenderer renderer in gobj.GetComponents<SpriteRenderer>())
+            {
+                if (renderer.name == "coralReefRenderer")
+                {
+                    return;
+                }
+            }
+
+            SpriteRenderer reefRenderer = gobj.AddComponent<SpriteRenderer>();
+            if (reefRenderer != null)
+            {
+                reefRenderer.name = "coralReefRenderer";
+
+                string style = "";
+                if (tile.data.Skin != SkinType.None || tile.data.Skin != SkinType.Default) style = tile.data.Skin.ToString().ToLower();
+                else if (tile.data.climate != TribeType.None || tile.data.climate != TribeType.Nature) style = tile.data.climate.ToString().ToLower();
+
+                reefRenderer.sprite = PolyMod.Registry.GetSprite("coralreef", style);
+                reefRenderer.sortingLayerID = -1327643303;
+                reefRenderer.sortingOrder = -1883;
+            }
+        }
+    }
+
+    public static void ShowCoral(Tile tile, bool show)
+    {
+        GameObject gobj = tile.gameObject;
+        if (gobj != null)
+        {
+            foreach (SpriteRenderer renderer in gobj.GetComponents<SpriteRenderer>())
+            {
+                if (renderer.name == "coralReefRenderer")
+                {
+                    renderer.enabled = show;
+                }
+            }
+        }
     }
 }
